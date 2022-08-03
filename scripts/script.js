48342a3d488dbf1650890575ed2388e634a92086
Oybek Narzullayev <oybeknarzullaev99@gmail.com>
2022-08-03 22:57:31 +0500

    1

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+var col = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < col.length; i++) {
+  col[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
